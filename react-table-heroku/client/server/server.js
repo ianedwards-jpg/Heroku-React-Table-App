939f56bc499546496dc8476983e31900b0a9f2e1
Ian Edwards <ian.edward@uconn.edu>
2020-08-07 17:28:33 -0400

    update

--- react-table-heroku/client/server/server.js
+++ react-table-heroku/client/server/server.js
@@ -3,10 +3,16 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+}
+
 app.listen(port, () => {
    console.log('Server is up!');
 });
